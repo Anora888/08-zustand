3cd7c0666134051c22b25f5dcfe62e70dbf87d9e
Anora888 <anoramaomao@gmail.com>
2026-02-04 16:51:15 +0200

    added solutions for tasks 1-4

--- app/notes/action/create/page.tsx
+++ app/notes/action/create/page.tsx
@@ -1,41 +1,21 @@
 import { Metadata } from "next";
-import NoteForm from "@/components/NoteForm/NoteForm";
 import { TAGS } from "@/types/note";
 import css from "./CreateNote.module.css";
+import CreateNoteClient from "./CreateNote.client";
 
 export const metadata: Metadata = {
   title: "Create note",
   description: "Create a new note in NoteHub.",
-  openGraph: {
-    title: "Create note",
-    description: "Create a new note in NoteHub.",
-    url: "",
-    images: [
-      {
-        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-        width: 1200,
-        height: 630,
-        alt: "NoteHub — a simple web-based note-taking application built with Next.js",
-      },
-    ],
-    type: "website",
-  },
 };
 
-const CreateNote = () => {
+export default function CreateNotePage() {
   return (
     <main className={css.main}>
       <div className={css.container}>
         <h1 className={css.title}>Create note</h1>
 
-        <NoteForm
-          categories={TAGS}
-          onCancel={() => {}}
-          onCreated={() => {}}
-        />
+        <CreateNoteClient categories={TAGS} />
       </div>
     </main>
   );
-};
-
-export default CreateNote;;
+}
